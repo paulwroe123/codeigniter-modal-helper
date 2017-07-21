363e3a4c713187fc93417b14258ab62bbab5c202
paulwroe123 <paul.wroe@bbc.co.uk>
2017-07-21 14:50:11 +0100

    Added head syntax

--- modal_helper.php
+++ modal_helper.php
@@ -5,33 +5,33 @@
 	Example Usage: <?= modal('modal.php', array('data-modal'=>'myModal')) ?>
 */
 
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+if (!defined('BASEPATH'))exit('No direct script access allowed');
 
 if (!function_exists('modal')) { 
 
 	function modal($path = NULL, $attributes = array()) {
-		
-		$modal = '';
 
 		$contentPath = APPPATH.'views/'.$path;
 		
 		if (file_exists($contentPath)) {
 			
-			$modal = '<div class="modal"';
-			
+			$_attributes = '';
+
 			foreach ($attributes as $key => $value) {
 
-				$modal .= " ".$key."='".$value."' ";
+				$_attributes .= " ".$key."='".$value."' ";
 
 			}
 
-			$modal .= '>';
+			$modal_content = file_get_contents($contentPath, FALSE);
 
-			$modal .= file_get_contents($contentPath, FALSE);
-
-			$modal .= '</div>';
-
-			return $modal;
+			return <<<MODAL
+				<div class='modal' {$_attributes}>
+					<div class='modal-content'>
+						{$modal_content}
+					</div>
+				</div>
+MODAL;
 
 		}
 
